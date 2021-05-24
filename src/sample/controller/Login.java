af6cee5dca000b0b0515194b98d7d41dec6a2448
marrebarre <martiin98@gmail.com>
2021-05-24 17:30:48 +0200

    Added login and add document functionality

--- src/sample/controller/Login.java
+++ src/sample/controller/Login.java
@@ -1,13 +1,33 @@
 package sample.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import sample.Main;
+import sample.model.Account;
 
 
 public class Login extends Controller {
 
+    @FXML
+    TextField usernameTxt;
+    @FXML
+    TextField passwordTxt;
+
     public void loginPressed() {
 
-        changeScene("journal.fxml");
+        Account account = new Account(usernameTxt.getText(), passwordTxt.getText());
+
+        String tmp = gson.toJson(account);
+
+        try {
+            String status = post("/login", tmp);
+
+            if (status.equals("OK")){
+                changeScene("journal.fxml");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
